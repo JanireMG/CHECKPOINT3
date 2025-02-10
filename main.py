617e897aff5f50a9b893b346470e7c8b843c2064
JanireMG <janiremartinez1997@gmail.com>
2025-02-10 10:52:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#INDEX
+word= "Hola"
+query= word.index("Hola")
+print(query)
+
+#FIND
+word= "Hola"
+query= word.find("Hola")
+print(query)
+
+#IN
+word= "Hola"
+query= "Hola" in word
+print(query)
+
+
+#REPLACE
+word= "Hola"
+word= word.replace("Hola", "Adios")
+print(word)
